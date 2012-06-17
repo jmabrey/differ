ce2a2075c154a8068534dcad7f63e4ce6724c13c
Joshua Mabrey <j_mabrey@yahoo.com>
2012-06-16 22:32:43 -0400

    Updated poms for readability and to fix source code version bug. Also added limited registration functionality

--- differ-app/src/main/java/cz/nkp/differ/gui/components/LoginRegisterComponent.java
+++ differ-app/src/main/java/cz/nkp/differ/gui/components/LoginRegisterComponent.java
@@ -1,10 +1,14 @@
 package cz.nkp.differ.gui.components;
 
+import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.LoginForm;
 import com.vaadin.ui.LoginForm.LoginListener;
 import com.vaadin.ui.VerticalLayout;
+
+import cz.nkp.differ.gui.windows.RegisterUserWindow;
+import cz.nkp.differ.util.GUIHelperFunctions;
 
 /**
  * Basic login form wrapper created so that adding customization to the form later would be much easier.
@@ -23,6 +27,9 @@ public class LoginRegisterComponent extends CustomComponent{
 		LoginForm loginForm = new LoginForm();
 		loginForm.addListener(parent);
 		layout.addComponent(loginForm);
+		Button registerButton = new Button("Register");
+		registerButton.addListener(GUIHelperFunctions.createWindowOpenButtonListener(new RegisterUserWindow()));
+		layout.addComponent(registerButton);
 		return layout;
 	}
 }
--- differ-app/src/main/java/cz/nkp/differ/gui/windows/RegisterUserWindow.java
+++ differ-app/src/main/java/cz/nkp/differ/gui/windows/RegisterUserWindow.java
@@ -1,12 +1,19 @@
 package cz.nkp.differ.gui.windows;
 
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
+import cz.nkp.differ.user.UserDataController;
 import cz.nkp.differ.util.GUIHelperFunctions;
 
 @SuppressWarnings("serial")
-public class RegisterUserWindow extends Window{
+public class RegisterUserWindow extends Window implements ClickListener{
 	public RegisterUserWindow(){
 		setCaption("Register User");
 		setModal(true);
@@ -14,9 +21,46 @@ public class RegisterUserWindow extends Window{
 		setResizable(false); 
 		center();
 		setWidth("25%");
-		Button close = new Button("Register");
+		
+		addComponent(createRegisterUserWindow());
+		
+		Button register = new Button("Register");
+		register.addListener(this);
+		addComponent(register);
+		
+		Button close = new Button("Close");
 		close.addListener(GUIHelperFunctions.createWindowCloseButtonListener(this));
+		
         addComponent(close); 
+	}
+	
+	
+	TextField nameField;
+	PasswordField passField;
+	/**
+	 * 
+	 * @return
+	 */
+	private Layout createRegisterUserWindow(){
+		
+		VerticalLayout layout = new VerticalLayout();
+		
+		nameField = new TextField("Username");
+		layout.addComponent(nameField);
+		
+		passField = new PasswordField("Password");
+		layout.addComponent(passField);
+		
+		return layout;
+	}
+
+	@Override
+	public void buttonClick(ClickEvent event) {
+		String nameValue = (String) nameField.getValue();
+		String passValue = (String) passField.getValue();
+		if(nameValue != null && passValue != null){
+			UserDataController.getInstance().addUser(nameValue, passValue);
+		}//TODO:password strength checking, closing dialog etc
 	}
 		
 }
