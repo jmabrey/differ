97a46d7459f57a8539683b0dac7ed592edc2a624
Joshua Mabrey <j_mabrey@yahoo.com>
2012-07-19 08:13:39 -0400

    Type based PluginPollingThread bug fixed

--- differ-plugins/differ-plugins-root/src/main/java/cz/nkp/differ/plugins/PluginPollingThread.java
+++ differ-plugins/differ-plugins-root/src/main/java/cz/nkp/differ/plugins/PluginPollingThread.java
@@ -6,7 +6,7 @@ import com.vaadin.ui.Component;
 
 public class PluginPollingThread extends Thread{
 	
-	private volatile Component comp;
+	private volatile Component comp = null;
 	private volatile PluginComponentReadyCallback callback;
 	private volatile DifferPluginInterface plugin;
 	
@@ -14,6 +14,9 @@ public class PluginPollingThread extends Thread{
 		if(plugin == null || callback == null){
 			throw new IOException("Plugin or callback null!");
 		}
+		
+		this.plugin = plugin;
+		this.callback = callback;
 	}
 	
 	volatile boolean continueRun = true;
